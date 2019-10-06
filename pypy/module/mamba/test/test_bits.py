a513a5e071a3bee56758e624f209f050c170cb3f
lc873 <lc873@cornell.edu>
2019-10-06 13:19:21 -0400

    [Mamba Test] tests for __str__ and __repr__

--- pypy/module/mamba/test/test_bits.py
+++ pypy/module/mamba/test/test_bits.py
@@ -7,3 +7,26 @@ class AppTestBits:
     def test_import_mamba(self):
         import mamba, sys
         assert 0 == 0
+
+    def test_bits_str(self):
+        import mamba, sys
+        b = mamba.Bits(8,42)
+        assert str(b) == '2a'
+        b = mamba.Bits(32,42)
+        assert str(b) == '0000002a'
+        b = mamba.Bits(32,48879)
+        assert str(b) == '0000beef'
+        b = mamba.Bits(512,13907095861846720239)
+        assert str(b) == '0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0ffee00deadbeef'
+
+    def test_bits_repr(self):
+        import mamba, sys
+        b = mamba.Bits(8,42)
+        assert repr(b) == 'Bits8( 0x2a )'
+        b = mamba.Bits(32,42)
+        assert repr(b) == 'Bits32( 0x0000002a )'
+        b = mamba.Bits(32,48879)
+        assert repr(b) == 'Bits32( 0x0000beef )'
+        b = mamba.Bits(512,13907095861846720239)
+        assert repr(b) == 'Bits512( 0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0ffee00deadbeef )'
+
