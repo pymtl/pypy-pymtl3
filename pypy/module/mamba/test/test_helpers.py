517127c5ef5bf3981a762da6e423aa28c90ee6e4
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2020-03-15 22:40:32 +0100

    a test for _rbigint_rshift_maskoff (passes)

--- pypy/module/mamba/test/test_helpers.py
+++ pypy/module/mamba/test/test_helpers.py
@@ -1,6 +1,7 @@
 from hypothesis import strategies, given, example, assume
 from rpython.rlib.rbigint     import rbigint, SHIFT, BASE8, BASE16
-from pypy.module.mamba.helper_funcs import setitem_long_long_helper
+from pypy.module.mamba.helper_funcs import (setitem_long_long_helper,
+        _rbigint_rshift_maskoff, _rbigint_rshift_maskoff_retint)
 
 @strategies.composite
 def setitem_inputs(draw):
@@ -19,6 +20,11 @@ def bitify(nbits, value):
     bits.extend(['0'] * (nbits - len(bits)))
     return bits
 
+def cmpbits(result, bits):
+    if type(result) in (int, long):
+        assert bin(result)[2:].zfill(len(bits)) == "".join(reversed(bits))
+    else:
+        assert result.format("01").zfill(len(bits)) == "".join(reversed(bits))
 
 @example((257, rbigint.fromlong(1 << (63 * 2)), rbigint.fromlong(1 << 63), 0, 127))
 @given(setitem_inputs())
@@ -32,6 +38,25 @@ def test_setitem_long_long_helper(input):
     assert stop - start == len(otherbits)
     bits[start: stop] = otherbits
     result = setitem_long_long_helper(value, other, start, stop)
-    assert result.format("01").zfill(len(bits)) == "".join(reversed(bits))
+    cmpbits(result, bits)
 
+@strategies.composite
+def rshift_maskoff_inputs(draw):
+    nbits = draw(strategies.integers(64, 1024))
+    # only big bits for now
+    value = rbigint.fromlong(draw(strategies.integers(min_value=0, max_value=(1<<nbits)-1)))
+    shamt = draw(strategies.integers(0, nbits - 1))
+    masklen = draw(strategies.integers(1, nbits - shamt))
+    return nbits, value, shamt, masklen
 
+@given(rshift_maskoff_inputs())
+def test_rshift_maskoff(input):
+    nbits, value, shamt, masklen = input
+    print nbits, value, shamt, masklen
+    bits = bitify(nbits, value)
+    resbits = bits[shamt: shamt + masklen]
+    res = _rbigint_rshift_maskoff(value, shamt, masklen)
+    cmpbits(res, resbits)
+    if masklen < SHIFT:
+        res = _rbigint_rshift_maskoff_retint(value, shamt, masklen)
+        cmpbits(res, resbits)
